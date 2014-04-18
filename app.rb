d81a839f45b158a42e85ac09427f9bb7b6635478
Bryan Bibat <bry@bryanbibat.net>
2014-04-19 02:26:02 +0800

    Prepare for pages pt2

--- app.rb
+++ app.rb
@@ -3,7 +3,8 @@ Bundler.require
 require 'sinatra/asset_pipeline'
 
 class App < Sinatra::Base
-  set :assets_host, 'bryanbibat.net'
+  set :assets_host, 'datenshizero.github.io'
+  set :assets_protocol, 'https'
   set :assets_css_compressor, :sass
   set :assets_js_compressor, :yui
 
